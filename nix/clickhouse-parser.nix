2395c4f71d0f3f9757b32ae406c591ad763c13f4
Dorran Howell <dorranh@appalachian.dev>
2025-04-21 13:51:49 +0200

    Fix path for extracting header files in Nix build

--- nix/clickhouse-parser.nix
+++ nix/clickhouse-parser.nix
@@ -1,20 +1,17 @@
 (final: prev: {
    clickhouse-parser-lib = (prev.clickhouse.override {rustSupport=false;}).overrideAttrs (oldAttrs: {
     buildPhase = ''
-        runHook preBuild
-
-        # Grab the relevant header files we need in our output since they do not seem to be available later.
+        # Note: buildPhase is run in cmake build directory not the repo root.
         mkdir -p headers
-        find src/Parsers -name '*.h' -exec cp {} ./headers/ \;
+        find ../src/Parsers -name '*.h' -exec echo "Found header file:" {} \; -exec cp {} ./headers/ \;
 
         # Build the static lib for the clickhouse SQL parser (libclickhouse_parsers)
         cmake --build . --target clickhouse_parsers --verbose
-        runHook postBuild
     '';
 
     installPhase = ''
-        mkdir -p $out/include/clickhouse/headers
-        cp headers/* $out/include/clickhouse/headers/
+        mkdir -p $out/include/clickhouse
+        cp -r headers $out/include/clickhouse
         cp src/Parsers/libclickhouse_parsers.a $out/include/clickhouse/
     '';
    });
